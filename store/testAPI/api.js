8cd841777116277a6ee176d4826609805749196e
Crowdbotics <engineering@crowdbotics.com>
2024-05-07 03:32:02 +0000

    Update store.

--- store/testAPI/api.js
+++ store/testAPI/api.js
@@ -1,155 +1,115 @@
-import axios from "axios";
+import axios from "axios"
 const testAPI = axios.create({
   baseURL: "https://test-47823.botics.co",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  }
-});
-
-function api_docs_schema_retrieve(payload) {
-  return testAPI.get(`/api-docs/schema/`, {
-    params: {
-      lang: payload.lang
-    }
-  });
-}
-
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 function api_v1_login_create(payload) {
-  return testAPI.post(`/api/v1/login/`, payload);
+  return testAPI.post(`/api/v1/login/`, payload)
 }
-
 function api_v1_signup_create(payload) {
-  return testAPI.post(`/api/v1/signup/`, payload);
+  return testAPI.post(`/api/v1/signup/`, payload)
 }
-
+function rest_auth_user_retrieve(payload) {
+  return testAPI.get(`/rest-auth/user/`)
+}
+function rest_auth_user_update(payload) {
+  return testAPI.put(`/rest-auth/user/`, payload)
+}
+function rest_auth_user_partial_update(payload) {
+  return testAPI.patch(`/rest-auth/user/`, payload)
+}
+function api_docs_schema_retrieve(payload) {
+  return testAPI.get(`/api-docs/schema/`, { params: { lang: payload.lang } })
+}
+function rest_auth_login_create(payload) {
+  return testAPI.post(`/rest-auth/login/`, payload)
+}
+function rest_auth_logout_create(payload) {
+  return testAPI.post(`/rest-auth/logout/`)
+}
 function modules_disqus_comment_list(payload) {
   return testAPI.get(`/modules/disqus/comment/`, {
-    params: {
-      page: payload.page,
-      page_size: payload.page_size
-    }
-  });
+    params: { page: payload.page, page_size: payload.page_size }
+  })
 }
-
 function modules_disqus_comment_create(payload) {
-  return testAPI.post(`/modules/disqus/comment/`, payload);
+  return testAPI.post(`/modules/disqus/comment/`, payload)
 }
-
+function rest_auth_registration_create(payload) {
+  return testAPI.post(`/rest-auth/registration/`, payload)
+}
+function rest_auth_password_reset_create(payload) {
+  return testAPI.post(`/rest-auth/password/reset/`, payload)
+}
+function rest_auth_password_change_create(payload) {
+  return testAPI.post(`/rest-auth/password/change/`, payload)
+}
 function modules_disqus_comment_like_list(payload) {
   return testAPI.get(`/modules/disqus/comment-like/`, {
-    params: {
-      page: payload.page,
-      page_size: payload.page_size
-    }
-  });
+    params: { page: payload.page, page_size: payload.page_size }
+  })
 }
-
 function modules_disqus_comment_like_create(payload) {
-  return testAPI.post(`/modules/disqus/comment-like/`, payload);
+  return testAPI.post(`/modules/disqus/comment-like/`, payload)
 }
-
-function modules_disqus_comment_like_retrieve(payload) {
-  return testAPI.get(`/modules/disqus/comment-like/${payload.id}/`);
-}
-
-function modules_disqus_comment_like_update(payload) {
-  return testAPI.put(`/modules/disqus/comment-like/${payload.id}/`, payload);
-}
-
-function modules_disqus_comment_like_partial_update(payload) {
-  return testAPI.patch(`/modules/disqus/comment-like/${payload.id}/`, payload);
-}
-
-function modules_disqus_comment_like_destroy(payload) {
-  return testAPI.delete(`/modules/disqus/comment-like/${payload.id}/`);
-}
-
 function modules_disqus_comment_retrieve(payload) {
-  return testAPI.get(`/modules/disqus/comment/${payload.id}/`);
+  return testAPI.get(`/modules/disqus/comment/${payload.id}/`)
 }
-
 function modules_disqus_comment_update(payload) {
-  return testAPI.put(`/modules/disqus/comment/${payload.id}/`, payload);
+  return testAPI.put(`/modules/disqus/comment/${payload.id}/`, payload)
 }
-
 function modules_disqus_comment_partial_update(payload) {
-  return testAPI.patch(`/modules/disqus/comment/${payload.id}/`, payload);
+  return testAPI.patch(`/modules/disqus/comment/${payload.id}/`, payload)
 }
-
 function modules_disqus_comment_destroy(payload) {
-  return testAPI.delete(`/modules/disqus/comment/${payload.id}/`);
+  return testAPI.delete(`/modules/disqus/comment/${payload.id}/`)
 }
-
-function rest_auth_login_create(payload) {
-  return testAPI.post(`/rest-auth/login/`, payload);
+function modules_disqus_comment_like_retrieve(payload) {
+  return testAPI.get(`/modules/disqus/comment-like/${payload.id}/`)
 }
-
-function rest_auth_logout_create(payload) {
-  return testAPI.post(`/rest-auth/logout/`);
+function modules_disqus_comment_like_update(payload) {
+  return testAPI.put(`/modules/disqus/comment-like/${payload.id}/`, payload)
 }
-
-function rest_auth_password_change_create(payload) {
-  return testAPI.post(`/rest-auth/password/change/`, payload);
+function modules_disqus_comment_like_partial_update(payload) {
+  return testAPI.patch(`/modules/disqus/comment-like/${payload.id}/`, payload)
 }
-
-function rest_auth_password_reset_create(payload) {
-  return testAPI.post(`/rest-auth/password/reset/`, payload);
+function modules_disqus_comment_like_destroy(payload) {
+  return testAPI.delete(`/modules/disqus/comment-like/${payload.id}/`)
 }
-
 function rest_auth_password_reset_confirm_create(payload) {
-  return testAPI.post(`/rest-auth/password/reset/confirm/`, payload);
+  return testAPI.post(`/rest-auth/password/reset/confirm/`, payload)
 }
-
-function rest_auth_registration_create(payload) {
-  return testAPI.post(`/rest-auth/registration/`, payload);
-}
-
 function rest_auth_registration_resend_email_create(payload) {
-  return testAPI.post(`/rest-auth/registration/resend-email/`, payload);
+  return testAPI.post(`/rest-auth/registration/resend-email/`, payload)
 }
-
 function rest_auth_registration_verify_email_create(payload) {
-  return testAPI.post(`/rest-auth/registration/verify-email/`, payload);
+  return testAPI.post(`/rest-auth/registration/verify-email/`, payload)
 }
-
-function rest_auth_user_retrieve(payload) {
-  return testAPI.get(`/rest-auth/user/`);
-}
-
-function rest_auth_user_update(payload) {
-  return testAPI.put(`/rest-auth/user/`, payload);
-}
-
-function rest_auth_user_partial_update(payload) {
-  return testAPI.patch(`/rest-auth/user/`, payload);
-}
-
 export const apiService = {
-  api_docs_schema_retrieve,
   api_v1_login_create,
   api_v1_signup_create,
+  rest_auth_user_retrieve,
+  rest_auth_user_update,
+  rest_auth_user_partial_update,
+  api_docs_schema_retrieve,
+  rest_auth_login_create,
+  rest_auth_logout_create,
   modules_disqus_comment_list,
   modules_disqus_comment_create,
+  rest_auth_registration_create,
+  rest_auth_password_reset_create,
+  rest_auth_password_change_create,
   modules_disqus_comment_like_list,
   modules_disqus_comment_like_create,
-  modules_disqus_comment_like_retrieve,
-  modules_disqus_comment_like_update,
-  modules_disqus_comment_like_partial_update,
-  modules_disqus_comment_like_destroy,
   modules_disqus_comment_retrieve,
   modules_disqus_comment_update,
   modules_disqus_comment_partial_update,
   modules_disqus_comment_destroy,
-  rest_auth_login_create,
-  rest_auth_logout_create,
-  rest_auth_password_change_create,
-  rest_auth_password_reset_create,
+  modules_disqus_comment_like_retrieve,
+  modules_disqus_comment_like_update,
+  modules_disqus_comment_like_partial_update,
+  modules_disqus_comment_like_destroy,
   rest_auth_password_reset_confirm_create,
-  rest_auth_registration_create,
   rest_auth_registration_resend_email_create,
-  rest_auth_registration_verify_email_create,
-  rest_auth_user_retrieve,
-  rest_auth_user_update,
-  rest_auth_user_partial_update
-};
+  rest_auth_registration_verify_email_create
+}
